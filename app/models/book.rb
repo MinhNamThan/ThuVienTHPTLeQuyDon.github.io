4043dfa050fbea1b62819e3e35d66a77b2cd7b1e
MinhNamThan <thanminhnam17022001@gmail.com>
2022-09-22 15:03:28 +0700

    fix delete book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-  has_one_attached :doc_file
-  has_many_attached :doc_file_pages
+  has_one_attached :doc_file, dependent: :destroy
+  has_many_attached :doc_file_pages, dependent: :destroy
   belongs_to :subject
   belongs_to :category
 
